6f3e6f041b588802314e74cc50efd5d88b374771
IndieHum <moein.fereyduni@gmail.com>
2024-07-11 19:56:32 +0430

    prettier

--- app.js
+++ app.js
@@ -2,34 +2,34 @@ const ColorButton = document.querySelectorAll(".color-btn");
 const Body = document.querySelector("body");
 
 function ButtonHandler(event) {
-  switch (event.target.dataset.color) {
-    case "yellow":
-      Body.style.backgroundColor = "yellow";
-      break;
-    case "green":
-      Body.style.backgroundColor = "green";
-      break;
-    case "red":
-      Body.style.backgroundColor = "red";
-      break;
-    case "blue":
-      Body.style.backgroundColor = "blue";
-      break;
-    case "random":
-      RandomColorPicker();
-      break;
-  }
+    switch (event.target.dataset.color) {
+        case "yellow":
+            Body.style.backgroundColor = "rgb(255,255,0)";
+            break;
+        case "green":
+            Body.style.backgroundColor = "rgb(0,255,0)";
+            break;
+        case "red":
+            Body.style.backgroundColor = "rgb(255,0,0)";
+            break;
+        case "blue":
+            Body.style.backgroundColor = "rgb(0,0,255)";
+            break;
+        case "random":
+            RandomColorPicker();
+            break;
+    }
 }
 
 function RandomColorPicker() {
-  const red = Math.round(Math.random() * 255);
-  const green = Math.round(Math.random() * 255);
-  const blue = Math.round(Math.random() * 255);
-  const RandomColor = `rgb(${red}, ${green}, ${blue})`;
+    const red = Math.round(Math.random() * 255);
+    const green = Math.round(Math.random() * 255);
+    const blue = Math.round(Math.random() * 255);
+    const RandomColor = `rgb(${red}, ${green}, ${blue})`;
 
-  Body.style.backgroundColor = RandomColor;
+    Body.style.backgroundColor = RandomColor;
 }
 
 ColorButton.forEach((button) =>
-  button.addEventListener("click", ButtonHandler)
+    button.addEventListener("click", ButtonHandler)
 );
